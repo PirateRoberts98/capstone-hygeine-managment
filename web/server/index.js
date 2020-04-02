8d5a659a5d1312a4df291e15bf8826a8db96ddee
mikemd833 <mdiep044@uottawa.ca>
2020-04-01 20:06:24 -0400

    Updated Changelog

--- web/server/index.js
+++ web/server/index.js
@@ -1,5 +1,13 @@
 'use strict';
 var admin = require('firebase-admin');
+var serviceAccount = require("../../../ceg4912project-firebase-adminsdk-8qvzs-d95f3a692a.json");
+// Get a database reference to our blog
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://ceg4912project.firebaseio.com/'
+  });
+var db = admin.database();
+var ref = db.ref("server/");
 
 var path = require('path');
 var http = require('http');
@@ -18,26 +26,34 @@ var app = expressAppConfig.getApp();
 
 // Initialize the Swagger middleware
 http.createServer(app).listen(serverPort, function () {
-    admin.initializeApp({
-        credential: admin.credential.applicationDefault(),
-        databaseURL: 'https://ceg4912project.firebaseio.com/'
-      });
     console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
     console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
     
 });
 
 app.post('/alert', function(req,res) {
-    console.log(req.body);
+    let alert = req.body;
     res.send('Received Alert');
+    var alertRef = ref.child("alert");
+    alertRef.push({
+        alert
+    });
 });
 
 app.post('/sensor/temperature', function(req,res) {
-    console.log(req.body);
-    res.send('Received Alert');
+    res.send('Received temperature sensor JSON');
+    let senTemp = req.body;
+    var senTempRef = ref.child("sensorTemp");
+    senTempRef.push({
+        senTemp
+    });
 });
 
 app.post('/sensor/pressure', function(req,res) {
-    console.log(req.body);
-    res.send('Received Alert');
+    res.send('Received pressure sensor JSON');
+    let senPressure = req.body;
+    var senPressureRef = ref.child("sensorPressure");
+    senPressureRef.push({
+        senPressure
+    });
 });
