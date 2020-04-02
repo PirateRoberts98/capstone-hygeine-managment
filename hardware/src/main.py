e60a9684a730d53c604943030ae7619679d53d09
Robert Conrad <rconr060@uottawa.ca>
2020-04-02 19:20:52 -0400

    WIP: Restrucutre design

--- hardware/src/main.py
+++ hardware/src/main.py
@@ -1,23 +1,55 @@
-#!/usr/local/bin/python
-# Developed by: Robert Conrad 
+#!/usr/local/bin/python3
 
-
+# External Imports 
 import time
-import api_call
-import pressure_sensor
 
-# TODO: Move to a seperate managed package
-def initialize_sensors():
-    return NotImplementedError
 
-# TODO: Move to a seperate managed package
-def initialize_api_handler():
-    return NotImplementedError
+import sensors.pressure_sensor as pressure
+import sensors.tempature_sensor as temp 
+def init_sensors():
+    pressure.initialize_sensor_platform()
+    p_sensor = pressure.PressureSensor()
+    t_sensor = temp.TempatureSensor()
+    return {"pressure":p_sensor,"tempature":t_sensor}
 
-def initialize_modules():
-    return NotImplementedError
+def cleanup_sensors():
+    pressure.cleanup_sensor_platform()
+    return 
 
-def initialize_cycle(pressure,alert_handler,sensor_handler):
+import modules.alert_module as alert 
+import modules.validation_module as valid
+def init_modules(api, buffer_size=20):
+    a_module = alert.AlertModule(buffer_size,api)
+    s_module = valid.SystemValidate(api)
+    return {"alert":a_module,"validate":s_module}
+
+import api.alert_api as a_api
+import api.pressure_api as p_api
+import api.tempature_api as t_api
+def init_api(base_uri="localhost:5000"):
+    alert_handler = a_api.AlertHandler(base_uri)
+    pressure_handler = p_api.PressureHandler(base_uri)
+    tempature_handler = t_api.TempatureHandler(base_uri)
+    return {"alert":alert_handler,"pressure":pressure_handler,"tempature":tempature_handler}
+
+
+def main():
+    try:
+
+        # Initialize the handlers for alert and sensor
+        apis = init_api()
+        modules = init_modules(apis["alert"])
+        sensors = init_sensors()
+        initialize_cycle(
+            sensors["pressure"],sensors["tempature"],
+            modules["alert"], modules["verify"],
+            apis["alert"],apis["pressure"],apis["tempature"]
+            )
+    except KeyboardInterrupt:
+        cleanup_sensors()
+
+
+def initialize_cycle(p_sensor, t_sensor , a_module , v_module , api_alert, api_pressure, api_tempature):
     i = 1
     while True:
         time.sleep(10)
@@ -28,24 +60,6 @@ def initialize_cycle(pressure,alert_handler,sensor_handler):
         if False:
             alert_handler.add_alert(True)
         sensor_handler.add_sensor_data(value)
-
-
-def main():
-    try:
-        pressure_sensor.initialize_sensor_platform()
-        pressure = pressure_sensor.PressureSensor()
-        # Initialize the handlers for alert and sensor
-        base_uri = "localhost:5000"
-        alert_handler = api_call.AlertHandler(base_uri)
-        sensor_handler = api_call.SensorHandler(base_uri)
-        modules = initialize_modules()
-        if  (
-            modules == NotImplementedError
-            ):
-            print("System is not implemented to full functionality") 
-        initialize_cycle(pressure,alert_handler,sensor_handler)
-    except KeyboardInterrupt:
-        pressure_sensor.cleanup_sensor_platform()
 
 if __name__ == "__main__":
     main()
---
+++ hardware/src/modules/validation_module.py
@@ -0,0 +1,16 @@
+
+
+
+class SystemValidate:
+    def __init__(self,api):
+        self.api = api 
+        return 
+
+    def validate_tempature(self,data):
+        return isinstance(data,float)
+
+    def validate_pressure(self ,data):
+        return isinstance(data,bool)
+
+    def validate_api_pressence(self,api):
+        return NotImplemented
