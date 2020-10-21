8aeb660233b8f1a55d419fbe36dbc5b9875d55bc
ScottFulton <sfult067@uottawa.ca>
2020-10-21 14:42:00 -0400

    Added web api send calls for all sensors

--- hardware/src/main.py
+++ hardware/src/main.py
@@ -46,31 +46,37 @@ def main():
     offline = True if args.offline else config["offline"]
     # TODO Log the config state to debug 
     user_info = api.User("John Doe") #TODO log user create to debug 
-    sensor_info = api.Sensor("Temperature")  #TODO log user create to debug 
+    temp_sensor_info = api.Sensor("Temperature")  #TODO log user create to debug
+    humidity_sensor_info = api.Sensor("Humidity")
+    pressure_sensor_info = api.Sensor("Pressure")
+    
     web_api = api.WebAPI(base_url=base_url,offline=offline)
     logging.info("TODO: This is a test") # TODO: Log info on Web API 
-    temp_humidity_sensor = TempAndHumiditySensor()
-    pressure_sensor = PressureSensor()
+    temp_humidity = temp_humidity_sensor.TempAndHumiditySensor()
+    pressure = pressure_sensor.PressureSensor()
     try:
         while True:
             time.sleep(3)
             if mock:
                 logging.info("Logging a mock sensor output")
-                web_api.send_data(user_info,sensor_info,'{{"timestamp":{},"value":{}}}'.format(
+                web_api.send_data(user_info,temp_sensor_info,'{{"timestamp":{},"value":{}}}'.format(
                         datetime.now(pytz.timezone("Canada/Eastern")),420
                     ))
             else:
-                if temp_humidity_sensor.read_from_sensor():
+                if temp_humidity.read_from_sensor():
                     logging.debug("Tempature:{} , Humidity:{}".format(\
-                        temp_humidity_sensor.tempVal,temp_humidity_sensor.humidityVal))
-                    web_api.send_data(user_info,sensor_info,"{{\"timestamp\":{},\"value\":{}}}".format(
-                        datetime.now(pytz.timezone("Canada/Eastern")),temp_humidity_sensor.tempVal
+                        temp_humidity.tempVal,temp_humidity.humidityVal))
+                    web_api.send_data(user_info,temp_sensor_info,"{{\"timestamp\":{},\"value\":{}}}".format(
+                        datetime.now(pytz.timezone("Canada/Eastern")),temp_humidity.tempVal
+                    ))
+                    web_api.send_data(user_info,humidity_sensor_info,"{{\"timestamp\":{},\"value\":{}}}".format(
+                        datetime.now(pytz.timezone("Canada/Eastern")),temp_humidity.humidityVal
                     ))
                 else:
                     logging.warning("Status Error, please ensure deivce is working correctly")
 
-                web_api.send_data(user_info,sensor_info,"{{\"timestamp\":{},\"value\":{}}}".format(
-                        datetime.now(pytz.timezone("Canada/Eastern")),pressure_sensor.read_from_sensor()
+                web_api.send_data(user_info,pressure_sensor_info,"{{\"timestamp\":{},\"value\":{}}}".format(
+                        datetime.now(pytz.timezone("Canada/Eastern")),pressure.read_from_sensor()
                     ))
     except KeyboardInterrupt:
         #Leave system as expected
