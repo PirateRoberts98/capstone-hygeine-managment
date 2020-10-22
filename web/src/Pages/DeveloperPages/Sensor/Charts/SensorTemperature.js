8f7b0316fa3ddd69f3dd353c152b2c23d86c5fe3
Mike D <mdiep044@uottawa.ca>
2020-10-22 02:18:47 -0400

    Fixed bug url

--- web/src/Pages/DeveloperPages/Sensor/Charts/SensorTemperature.js
+++ web/src/Pages/DeveloperPages/Sensor/Charts/SensorTemperature.js
@@ -20,7 +20,7 @@ class SensorTemperatureChart extends React.Component {
         var data = {
             type: "Temperature"
         };
-        var request = new Request('http://ec2-35-182-173-184.ca-central-1.compute.amazonaws.com:3001/api/postsensordata', {
+        var request = new Request('http://ec2-35-182-173-184.ca-central-1.compute.amazonaws.com:3001/api/getsensordata', {
             method: 'POST',
             headers: new Headers({ 'Content-Type' : 'application/json' }),
             body: JSON.stringify(data)
