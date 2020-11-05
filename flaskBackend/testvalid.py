10ddb5ce698cf3da6911b36abe9ecd77431c9758
Nikita Bliumkin <nbliu026@uottawa.ca>
2020-11-05 14:52:39 -0500

    fix

--- flaskBackend/testvalid.py
+++
@@ -1,9 +0,0 @@
-def testvalid(data):
-    try:
-        print("data:")
-        print(data)
-        if 'user' in data:
-            print("user exists")
-        return data
-    except:
-        raise Exception('ERROR POST SensorData')
