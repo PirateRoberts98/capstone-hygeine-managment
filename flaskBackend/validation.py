dc4be8f8b95bbefea248072f9ace730822ee4ec7
Nikita Bliumkin <nbliu026@uottawa.ca>
2020-11-07 15:44:58 -0500

    fix

--- flaskBackend/validation.py
+++ flaskBackend/validation.py
@@ -2,12 +2,11 @@ import json
 
 def validate_data(obj):
     try:
-        print('start')
+        
         output = {
             'status' : 201,
             'msg' : None
         }
-        print(output['status'])
 
         #validating device id
         #if 'device_id' in self.obj:
@@ -37,11 +36,8 @@ def validate_data(obj):
 
         #validating user
         if 'user' in obj:
-            print('user')
             if 'id' in obj['user']:
-                print('in')
                 if type(obj['user']['id']) is str:
-                    print('type')
                     if output['status'] == 201:
                         output['msg'] = 'Validated'
                 else:
@@ -57,11 +53,8 @@ def validate_data(obj):
         
         #validating sensor
         if 'sensor' in obj:
-            print('sensor')
             if 'type' in obj['sensor']: 
-                print('in')
                 if type(obj['sensor']['type']) is str: 
-                    print('type')
                     if output['status'] == 201:
                         output['msg'] = 'Validated' 
                 else:
@@ -76,32 +69,23 @@ def validate_data(obj):
 
         #validating data
         if 'data' in obj:
-            print('data')
             if 'timestamp' in obj['data'] and 'value' in obj['data']:
-                print('timestamp')
                 if type(obj['data']['timestamp']) is float:
-                    print('type')
-                    if obj['sensor']['type'] == 'Humidity':
-                        print('sensor type')
-                        
+                    if obj['sensor']['type'] == 'Humidity':        
                         if obj['data']['value'] > 0:
-                            print('>')
                             if output['status'] == 201:
                                 output['msg'] = 'Validated'
                         else:
                             output['status'] = 404
                             output['msg'] = 'Fail vaildation' 
                     elif obj['sensor']['type'] == 'Temperature':  
-                        print('sensor type')
                         temp = int(obj['data']['value'])
                         if temp > 0:
-                            print('>')
                             output['msg'] = 'Validated'
                         else:
                             output['status'] = 404
                             output['msg'] = 'Fail vaildation' 
                     elif obj['sensor']['type'] == 'Pressure': 
-                        print('sensor type') 
                         if obj['data']['value'] is True or obj['data']['value'] is False:
                             print('is')
                             output['msg'] = 'Validated'
@@ -124,7 +108,6 @@ def validate_data(obj):
         resp = json.dumps(output)
         return resp
     except:
-        print("test")
         raise Exception('ERROR POST SensorData')
 
 
