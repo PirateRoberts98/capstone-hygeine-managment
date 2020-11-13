67e18155ce60656d0f2244fa86d6a50d36dd5a1e
Mike D <mdiep044@uottawa.ca>
2020-11-12 20:05:26 -0500

    Added UI Components to Patient View - Contact Doctor

--- web/src/Pages/PatientPages/ContactDoctor/index.js
+++ web/src/Pages/PatientPages/ContactDoctor/index.js
@@ -6,6 +6,13 @@ import PageTitle from '../../../Layout/AppMain/PageTitle';
 import AppHeader from '../../../Layout/AppHeader';
 import AppSidebar from '../../../Layout/AppSidebar';
 
+// Material UI
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import Icon from '@material-ui/core/Icon';
+
 const ContactDoctor = ({match}) => (
     <Fragment>
         <AppHeader/>
@@ -23,11 +30,32 @@ const ContactDoctor = ({match}) => (
                             transitionLeave={false}>
                             <div>
                                 <PageTitle
-                                    heading="Contact Doctor"
-                                    subheading="Contact a general physcian or your doctor below."
+                                    heading="Contact Caregiver"
+                                    subheading="Contact your caregiver or your doctor below."
                                     icon="pe-7s-user icon-gradient bg-mean-fruit"
                                 />
                             </div>
+                            <Typography variant="h2" gutterBottom>
+                                Contact Caregiver
+                            </Typography>
+                            <form>
+                            <TextField
+                                id="outlined-multiline-static"
+                                label="Message"
+                                multiline
+                                rows={4}
+                                defaultValue=""
+                                variant="outlined"
+                            />
+                            <Button
+                                variant="contained"
+                                color="primary"
+                                //className={classes.button}
+                                endIcon={<Icon>send</Icon>}
+                            >
+                                Send
+                            </Button>
+                            </form>
                         </ReactCSSTransitionGroup>
                     </Fragment>
                 </div>
