402521294aa43ea8c109f887be023cc0d02ccf9f
mikemd833 <mdiep044@uottawa.ca>
2020-11-26 16:36:40 -0500

    Linked Messages component with Database

--- web/src/components/MessagesComponent.js
+++ web/src/components/MessagesComponent.js
@@ -9,17 +9,52 @@ class MessagesComponent extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-
+            messagesArray: []
         }
     }
 
     componentDidMount(){
+        this.retrieveMessages()
+        setInterval(
+            this.retrieveMessages(),
+            5000
+        );
+    }
+
+    retrieveMessages = () => {
+        var tht = this;
+        var request = new Request(awsConnection.awsEC2Connection+'/api/getMessages/1', {
+            method: 'GET',
+        });
+        fetch(request).then(function(response) {
+            response.json()
+                .then(function(data){
+                    if(data){
+                        // Format into JSX
+                        let msgArray = [];
+                        data.map((item,index) => {
+                            let msg = <Card className="main-card mb-3"><CardBody><Row key={index} form>From: {item.senderId[index]}. Message: {item.message[index]}.</Row></CardBody></Card>;
+                            msgArray.push(msg);
+                        });
+
+                        return msgArray;
+                    }
+                })
+                .then((msgArray)=>{
+                    tht.setState({
+                        messagesArray: msgArray
+                    });
+                })
+                .catch(function(err){
+                    console.log(err)
+                });
+        });
     }
 
     render() {
         return (
             <div>
-                <Card className="main-card mb-3"><CardBody><Row key='1' form>Message: </Row></CardBody></Card>
+                {this.state.messagesArray}
             </div>
         )
     }
