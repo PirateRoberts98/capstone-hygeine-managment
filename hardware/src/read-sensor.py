30b6ceb0a413804089e7278e685a4afe91be38b7
ScottFulton <sfult067@uottawa.ca>
2020-12-01 03:24:35 -0500

    Added read sensor class

---
+++ hardware/src/read-sensor.py
@@ -0,0 +1,42 @@
+import  time
+import board
+import busio
+
+import adafruit_mpr121
+
+class ReadSensor:
+    def __init__(self,pin,addr=90):
+        self.pin = pin
+        self.i2c = busio.I2C(board.SCL, board.SDA)
+        self.buffer = bytearray(2)
+        self.addr = addr
+        mpr121 = adafruit_mpr121.MPR121(self.i2c)
+        self.touched = self.read_from_sensor()
+    
+    def read_from_sensor(self):
+        self.touched = 0
+        self.i2c.readfrom_into(90, self.buffer)
+        value = self.buffer[1] << 8 | self.buffer[0]
+        if(value & (1 << (self.pin))):
+           self.touched = 1
+        
+        
+        return self.touched
+
+def main():
+    sensor = ReadSensor(0)
+    try:
+        while True:
+            data = sensor.read_from_sensor()
+            if(data == 1):
+                print("Under Pressure")
+            else:
+                print("Not Under Pressure")
+            time.sleep(0.3)
+    except KeyboardINterrupt:
+        pass
+
+if __name__ == "__main__":
+    main()
+
+
