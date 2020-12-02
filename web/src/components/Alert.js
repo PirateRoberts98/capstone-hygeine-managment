65d86c447bd50d26f965b8b25d06bbf32a1a447d
mikemd403 <mikemd833@gmail.com>
2020-12-01 19:55:56 -0500

    Removed Firebase components from Alert component

--- web/src/components/Alert.js
+++ web/src/components/Alert.js
@@ -24,36 +24,6 @@ class Alert extends React.Component {
             this.retrieveDataPressure();
             this.retrieveDataTemperature();
         }, 60000);
-
-        /*database.ref('server/alert').on('value', (snapshot) => {
-            if(this.state.alerts != snapshot.val() && snapshot.val() != null){
-                let alertDataObjects = snapshot.val();
-                let alertDataArray = [];
-                let alertSeverityArray = [];
-                let alertWarningMessage = [];
-                let key;
-                for(let i=0; i < Object.keys(alertDataObjects).length; i++){
-                    key = Object.keys(alertDataObjects)[i];
-                    alertDataArray.push(alertDataObjects[key]);
-                }
-                alertDataArray.map(item => {
-                    if(item != undefined) {
-                        alertSeverityArray.push(item.alert.severity);
-                        alertWarningMessage.push(item.alert.warning_message);
-                    }
-                });
-                // Format into JSX
-                let msgArray = []
-                for(let j=alertWarningMessage.length-1; j > -1; j--) {
-                    let msg = <Card className="main-card mb-3"><CardBody><Row key={j} form>Alert with severity: {alertSeverityArray[j]}. The warning message: {alertWarningMessage[j]}.</Row></CardBody></Card>;
-                    msgArray.push(msg);
-                }
-                this.setState({
-                    alerts: alertDataObjects,
-                    alertArray: msgArray,
-                });
-            }
-        });*/
     }
 
     retrieveDataHumdity = () => {
