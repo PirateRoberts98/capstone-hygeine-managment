11ca680ed1307dbdfe0d8b214bc1d37de6a01e66
mikemd403 <mikemd833@gmail.com>
2020-12-01 21:33:38 -0500

    Added DB query to get all users

--- database/queryDB.sql
+++ database/queryDB.sql
@@ -5,3 +5,9 @@ INSERT INTO users(fname, lname, email, doctor, bday, gender, pw, iscaregiver)
 /* Register User to DB*/
 INSERT INTO users (userID, fname, lname, bday, gender, doctor, isCaregiver, isPatient, isDeveloper, email, pw)
 VALUES (1, 'James', 'Lee', '12/12/1997', 'male', 'James Chui', false, false, true, 'developer@hms.com', 'pass123')
+
+/* Query users in DB*/
+SELECT * FROM users
+
+/* Query users in DB*/
+SELECT * FROM users
